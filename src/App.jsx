80db0fa1e301fb7135dd443ebfe626896a93ff9e
Sara han <hans63237@gmail.com>
2025-04-12 13:45:28 +0900

    zustand counter

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import CounterBox from './component/CounterBox'
+
+function App() {
+
+
+  return (
+    <div className='app-main'>
+      <h1>COUNTER</h1>
+    <CounterBox/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/CounterBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import counterStore from '../store/counterStore'
+
+const CounterBox = () => {
+    const {count, increase,increaseBy,decrease,decreaseBy,reset}=counterStore()
+  return (
+    <div className='main'>
+        <div className='count-box'>
+            <span onClick={reset}>초기화</span>
+            <h1>
+                {count}
+            </h1>
+            <div className='btn-area'>
+                <button onClick={increase}>+</button>
+                <button onClick={()=>increaseBy(10)}>+10</button>
+                <button onClick={decrease}>-</button>
+                <button onClick={()=>decreaseBy(10)}>-10</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CounterBox
